f48c0755ce867eabe7ce447c23ed914c695ce5de
takao.miyake <myktko.au@ezweb.ne.jp>
2020-08-19 09:34:13 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takao/miyake/qa_app0817/Answer.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.takao.miyake.qa_app0817
+
+import java.io.Serializable
+
+class Answer(val body: String, val name: String, val uid: String, val answerUid: String) : Serializable
